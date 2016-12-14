611bce89393d67e0da1e4c59ea9c9c10d3ca9a4d
Guen <guen0610j@gmail.com>
2016-12-15 01:57:40 +0800

    uart frame error
    UART@Frame@Error:@»#ál
    UART`Frame@Error:`¼#ñl

--- AVRGCC1.c
+++ AVRGCC1.c
@@ -4,8 +4,10 @@ Author:   Peter Fleury <pfleury@gmx.ch>   http://tinyurl.com/peterfleury
 File:     $Id: test_uart.c,v 1.7 2015/01/31 17:46:31 peter Exp $
 Software: AVR-GCC 4.x
 Hardware: AVR with built-in UART/USART
+
 DESCRIPTION:
           This example shows how to use the UART library uart.c
+
 *************************************************************************/
 #include <stdlib.h>
 #include <avr/io.h>
@@ -13,11 +15,11 @@ DESCRIPTION:
 #include <avr/pgmspace.h>
 
 #include "uart.h"
-
-
+#include "lcd.h"
+#define __AVR_ATmega128A__
 /* define CPU frequency in Hz in Makefile */
 #ifndef F_CPU
-#error "F_CPU undefined, please define CPU frequency in Hz in Makefile"
+#define F_CPU 1000000UL
 #endif
 
 /* Define UART buad rate here */
@@ -26,11 +28,22 @@ DESCRIPTION:
 
 int main(void)
 {
+	DDRC = 0xFF;    // lcd pins
+	// configure the microprocessor pins for the control lines
+    lcd_E_ddr |= (1<<lcd_E_bit);                    // E line - output
+    lcd_RS_ddr |= (1<<lcd_RS_bit);                  // RS line - output
+ 
+    lcd_RS_port &= ~(1<<lcd_RS_bit);
+	
     unsigned int c;
     char buffer[7];
     int  num=134;
 
-    
+    lcd_init_4d();
+	lcd_write_instruction_4d(lcd_Clear);
+    _delay_ms(10);
+    lcd_write_string_4d("main");
+
     /*
      *  Initialize UART library, pass baudrate and AVR cpu clock
      *  with the macro 
@@ -38,7 +51,7 @@ int main(void)
      *  or 
      *  UART_BAUD_SELECT_DOUBLE_SPEED() ( double speed mode)
      */
-    uart_init( UART_BAUD_SELECT(UART_BAUD_RATE,F_CPU) ); 
+    uart1_init( UART_BAUD_SELECT(UART_BAUD_RATE,F_CPU) ); 
     
     /*
      * now enable interrupt, since UART library is interrupt controlled
@@ -52,12 +65,12 @@ int main(void)
      *  uart_puts() blocks if it can not write the whole string to the circular 
      *  buffer
      */
-    uart_puts("String stored in SRAM\n");
+    uart1_puts("String stored in SRAM\n");
     
     /*
      * Transmit string from program memory to UART
      */
-    uart_puts_P("String stored in FLASH\n");
+    uart1_puts_P("String stored in FLASH\n");
     
         
     /* 
@@ -65,13 +78,13 @@ int main(void)
      * before transmitting via UART
      */     
     itoa( num, buffer, 10);   // convert interger into string (decimal format)         
-    uart_puts(buffer);        // and transmit string to UART
+    uart1_puts(buffer);        // and transmit string to UART
 
     
     /*
      * Transmit single character to UART
      */
-    uart_putc('\r');
+    uart1_putc('\r');
     
     for(;;)
     {
@@ -82,7 +95,7 @@ int main(void)
          * UART_NO_DATA is returned when no data is available.
          *
          */
-        c = uart_getc();
+        c = uart1_getc();
         if ( c & UART_NO_DATA )
         {
             /* 
@@ -98,7 +111,7 @@ int main(void)
             if ( c & UART_FRAME_ERROR )
             {
                 /* Framing Error detected, i.e no stop bit detected */
-                uart_puts_P("UART Frame Error: ");
+                uart1_puts_P("UART Frame Error: ");
             }
             if ( c & UART_OVERRUN_ERROR )
             {
@@ -107,7 +120,7 @@ int main(void)
                  * not read by the interrupt handler before the next character arrived,
                  * one or more received characters have been dropped
                  */
-                uart_puts_P("UART Overrun Error: ");
+                uart1_puts_P("UART Overrun Error: ");
             }
             if ( c & UART_BUFFER_OVERFLOW )
             {
@@ -115,12 +128,12 @@ int main(void)
                  * We are not reading the receive buffer fast enough,
                  * one or more received character have been dropped 
                  */
-                uart_puts_P("Buffer overflow error: ");
+                uart1_puts_P("Buffer overflow error: ");
             }
             /* 
              * send received character back
              */
-            uart_putc( (unsigned char)c );
+            uart1_putc( (unsigned char)c );
         }
     }
     
--- Debug/makedep.mk
+++ Debug/makedep.mk
@@ -4,3 +4,5 @@
 
 AVRGCC1.c
 
+uart.c
+
